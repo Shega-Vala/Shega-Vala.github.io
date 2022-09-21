3057032132782d75faaaa3a3bc84fe228f9b3977
Shega <104076420+Shega-Vala@users.noreply.github.com>
2022-09-21 17:35:39 -0400

    zoe website

---
+++ Zoe/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        autoControls: true,
+        slideWidth: 250,
+        slideMargin: 5,
+        randomStart: true,
+        captions: true,
+        speed:2000,
+        pagerType:'short',
+        pagerSelector:'#id_pager',
+
+
+    });
+   
+
+});
